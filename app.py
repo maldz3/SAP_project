8288b94cf2f10c16a51ee2c01a229810e6017c74
Maliha <maliha@Malihas-MacBook-Pro.local>
2021-07-07 21:26:26 -0700

    Implement 4 routes with integer functions

--- app.py
+++ app.py
@@ -4,8 +4,49 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def hello():
+    return "Hello World!"
+
+
+@app.route('/<int:number>')
+def all_integers(number):
+    string = ""
+    for i in range(1, number+1):
+        string += str(i) + " "
+    return string
+
+
+@app.route('/<int:number>/odd')
+def odd_integers(number):
+    string = ""
+    for i in range(1, number+1, 2):
+        string += str(i) + " "
+    return string
+
+
+@app.route('/<int:number>/even')
+def even_integers(number):
+    string = ""
+    if number > 1:
+        for i in range(2, number+1, 2):
+            string += str(i) + " "
+    return string
+
+
+@app.route('/<int:number>/prime')
+def prime_integers(number):
+    string = ""
+    for i in range(1, number+1):
+        if is_prime(i):
+            string += str(i) + " "
+    return string
+
+
+def is_prime(num):
+    for i in range(2, num//2 + 1):
+        if num % i == 0:
+            return False
+    return True
 
 
 if __name__ == '__main__':
